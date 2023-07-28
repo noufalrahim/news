0a52048dfb8163d29c4fa73d56f300b46c672829
noufalrahim <noufalrahim6784@gmail.com>
2023-07-28 08:23:53 +0530

    seventh commit

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,36 +1,26 @@
 import "./Home.css"
 import axios from "axios";
-import {useEffect, useState, useRef } from "react"
+import {useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 import Loading from "../UI/LoadingSpinner/Loading";
 function Home() {
     const [error, setError] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [news, setNews] = useState([]);
-    const [search, setSearch] = useState("Google") 
-    const inputRef = useRef();
     const date = new Date()
-
-    function searcHandler(){
-        setSearch(inputRef.current.value)
-        console.log(inputRef.current.value)
-    }
 
     useEffect(() => {
         async function FetchNews() {
             setIsLoading(true)
-            const response = await axios.get(`https://newsapi.org/v2/everything?q=${search}&from=${date.getFullYear()}-${date.getMonth()}-${date.getDate()}&to=${date.getFullYear()}-${date.getMonth()}-${date.getDate()}&sortBy=popularity&apiKey=291d6b9c094142b4907fca124c03e2c7`)
-            console.log(response.data.articles)
-            const responseData = response.data.articles
+            const response = await axios.get("https://newsapi.org/v2/everything?q=all&from=${date.getFullYear()}-${date.getMonth()}-${date.getDate()}&to=${date.getFullYear()}-${date.getMonth()}-${date.getDate()}&sortBy=popularity&apiKey=291d6b9c094142b4907fca124c03e2c7")
+            const responseData = response.data.foundNews
             setIsLoading(false)
             setNews(responseData)
         }
         FetchNews().catch((err) => {
             setError(true)
         });
-    }, [search])
-
-    
+    }, [])
     return (
         <>
             <div className="head">
@@ -46,13 +36,6 @@ function Home() {
             {isLoading ? <div><Loading/></div> : error ?<p className="failed">Server Error..! Failed to Fetch News!</p> : 
             <div className="container">
                 <div className="row">
-                
-                {/* <input className="input" id="search" placeholder="Search here: Biden"/> */}
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="Search" ref={inputRef}/>
-                            <label className="label typewriter" for="floatingInput"><p className="txt-move">Search</p></label>
-                            <div className="sbtn"><button className="s-btn" onClick={searcHandler}><i class="fas fa-solid fa-magnifying-glass">Search</i></button></div>
-                        </div>
                 {news.map((newsItem) => (
                     <div className="col-sm-1 col-md-6 col-lg-3">
                         <div className="card">
@@ -60,7 +43,7 @@ function Home() {
                             <div className="card-body">
                                 <h5 className="card-title">{newsItem.title}</h5>
                                 <p className="card-text">{newsItem.content.slice(0,-15)}... <Link to={newsItem.url}>Click here</Link></p>
-                                    <p class="card-text"><small class="text-muted">{newsItem.author}</small></p>
+                                    <p className="card-text"><small className="text-muted">{newsItem.author}</small></p>
 
 
                             </div>
---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require("express")
+const axios = require('axios');
+const app = express();
+var cors = require('cors');
+app.use(express.json());
+app.use(cors());
+main().catch(err => console.log(err));
+async function main() {
+    const date = new Date()
+    app.get("/", async function (req, res) {
+        let url = process.env.URL;
+        const response = await axios.get(url)
+        const responseData = response.data.articles
+        res.json({foundNews : responseData})
+    })
+    app.listen(8000, async function () {
+        console.log("Server is running on port : 8000");
+
+    })
+
+}
+
+
